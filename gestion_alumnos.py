a13143074f7e37ebba3c2ad19ae52f3975bc3fb3
CGBarrientos <cgbarrientos91@gmail.com>
2025-03-21 23:07:38 -0300

    guardado del tp

---
+++ gestion_alumnos.py
@@ -0,0 +1,46 @@
+# """"""""""""""""""""""""""""GESTION DE ALUMNOS"""""""""""""""""""""""""""""""""
+# datos = {
+#     "alumnos" = []
+# }
+
+# # Agregar alumno al listado
+# def agregar_alumno(nombre, apellido, dni, fecha_nacimiento, tutor):
+#     alumno = {
+#         "Nombre": nombre,
+#         "Apellido": apellido,
+#         "DNI": dni,
+#         "Fecha de nacimiento": fecha_nacimiento,
+#         "Tutor": tutor,
+#         "Notas": [],
+#         "Faltas": 0,
+#         "Amonestaciones": 0
+#     }
+#     datos["alumnos"].append(alumno)
+#     print(f"Alumno {Nombre} {Apellido} agregado")
+
+# #Mostrar Alumnos
+# def mostrar_alumnos():
+#     for iterador in range (len(datos["alumnos"])):
+#         print(f"Alumno {iterador+1}:")
+#         print(f"Nombre: {alumno['Nombre']}")
+#         print(f"Apellido: {alumno['Apellido']}")
+#         print(f"DNI: {alumno['DNI']}")
+#         print(f"Fecha de nacimiento: {alumno['Fecha de nacimiento']}")
+#         print(f"Tutor: {alumno['Tutor']}")
+#         print(f"Notas: {alumno['Notas']}")
+#         print(f"Faltas: {alumno['Faltas']}")
+#         print(f"Amonestaciones: {alumno['Amonestaciones']}")
+
+# #Modificar Datos de Alumno
+# def modificar_datos(indice, clave, valor):
+#     if indice < len(datos["Alumnos"]):
+#         datos["Alumnos"][indice][clave] = valor
+#     else:
+#         print("Indice invalido")
+
+# Expulsar alumno
+def expulsar_alumno(alumno):
+    if alumno < len(datos["Alumnos"]):
+        datos["Alumnos"].pop(indice)
+    else:
+        print("Indice invalido")
